3313f11353efb4197c69a354b7a7dd9cddaa1d90
Bastian <bquilitz@gmail.com>
2010-07-06 16:11:07 +0200

    added plugin files

---
+++ init.rb
@@ -0,0 +1,2 @@
+require "pingr-rails"
+
---
+++ lib/pingr-rails.rb
@@ -0,0 +1,40 @@
+require 'yaml'
+require 'pingdom'
+
+module PingrRails
+
+
+  CONFIG_DIR = File.join(RAILS_ROOT,'config','pingdom.yml')
+
+  class << self
+     attr_reader :configuration
+   end
+
+
+   def self.load_config(config_path)
+     @config_path = config_path
+     @configuration =  YAML.load_file(config_path)
+     @p = Pingdom.new(@configuration[:username], @configuration[:password], @configuration[:api_key])
+   end
+
+  def self.reload!
+    load_config(@config_path)
+  end
+
+
+  private
+    def self.method_missing(method, *args, &block)
+      throw "Error loading config!" if @p.nil? 
+      @p.send(method, *args, &block)
+    end
+
+end
+
+PingrRails.load_config(PingrRails::CONFIG_DIR)
+
+
+if RAILS_ENV == 'development'
+  ActionController::Base.class_eval do
+    append_before_filter { PingrRails.reload! }
+  end
+end
